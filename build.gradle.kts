f92000faf1b7896522a1614e164c56b958e51554
Evgeny Shilov <evgeny.shilov.225@gmail.com>
2022-05-03 01:15:23 +0500

    Add meme entity with migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.postgresql:postgresql")
---
+++ src/main/resources/db/migration/V1__Create_memes_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE memes (
+    id bigserial PRIMARY KEY,
+    content varchar(1000),
+    viewed boolean,
+    last_checked timestamptz
+);
