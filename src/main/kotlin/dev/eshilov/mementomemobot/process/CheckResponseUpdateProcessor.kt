a3eefdf0a6ff8f4a0e05e3a8a28beb7f4aa10fb0
Evgeny Shilov <evgeny.shilov.225@gmail.com>
2022-05-03 22:09:51 +0500

    Implement check response update processor

---
+++ src/main/kotlin/dev/eshilov/mementomemobot/process/CheckResponseUpdateProcessor.kt
@@ -0,0 +1,96 @@
+package dev.eshilov.mementomemobot.process
+
+import dev.eshilov.mementomemobot.CheckAnswer
+import dev.eshilov.mementomemobot.config.AppProps
+import dev.eshilov.mementomemobot.meme.MemeEntity
+import dev.eshilov.mementomemobot.meme.MemeRepository
+import dev.eshilov.mementomemobot.telegram.operation.BotApi
+import dev.eshilov.mementomemobot.telegram.model.AnswerCallbackQueryRequest
+import dev.eshilov.mementomemobot.telegram.model.DeleteMessageRequest
+import dev.eshilov.mementomemobot.telegram.model.SendMessageRequest
+import dev.eshilov.mementomemobot.telegram.model.Update
+import org.springframework.stereotype.Component
+
+@Component
+class CheckResponseUpdateProcessor(
+    private val botApi: BotApi,
+    private val appProps: AppProps,
+    private val memeRepository: MemeRepository
+) {
+    fun processCheckResponse(update: Update) {
+        if (isYesCheckResponse(update)) {
+            handleYesCheckResponse(update)
+        } else {
+            handleNoCheckResponse(update)
+        }
+
+        deleteCheckMessage(update)
+    }
+
+    private fun isYesCheckResponse(update: Update): Boolean {
+        val answer = update.callbackQuery?.data!!
+        return answer == CheckAnswer.YES.key
+    }
+
+    private fun handleYesCheckResponse(update: Update) {
+        sendYesAnswerCallbackQuery(update)
+        val meme = findMemeForCheckResponseUpdate(update)
+        meme?.let {
+            markMemeAsChecked(meme)
+            notifySenderThatMemeViewed(meme)
+        }
+    }
+
+    private fun sendYesAnswerCallbackQuery(update: Update) {
+        val callbackQueryId = getCallbackQueryId(update)
+        botApi.answerCallbackQuery(
+            AnswerCallbackQueryRequest(
+                callbackQueryId = callbackQueryId,
+                text = "Умничка!!!"
+            )
+        )
+    }
+
+    private fun findMemeForCheckResponseUpdate(update: Update): MemeEntity? {
+        val checkMessageId = getCheckMessageId(update)
+        return memeRepository.findOneByLastCheckMessageId(checkMessageId)
+    }
+
+    private fun markMemeAsChecked(meme: MemeEntity) {
+        meme.viewed = true
+        meme.lastCheckMessageId = null
+        memeRepository.save(meme)
+    }
+
+    private fun notifySenderThatMemeViewed(meme: MemeEntity) {
+        botApi.sendMessage(
+            SendMessageRequest(
+                chatId = appProps.receiverChatId,
+                text = "Котик посмотрел мем ${meme.content}",
+                replyMarkup = null
+            )
+        )
+    }
+
+    private fun handleNoCheckResponse(update: Update) {
+        botApi.answerCallbackQuery(
+            AnswerCallbackQueryRequest(
+                callbackQueryId = getCallbackQueryId(update),
+                text = "Ну хорошо, только потом не забудь!!!"
+            )
+        )
+    }
+
+    private fun getCallbackQueryId(update: Update) = update.callbackQuery!!.id
+
+    private fun getCheckMessageId(update: Update) = update.callbackQuery!!.message!!.messageId
+
+    private fun deleteCheckMessage(update: Update) {
+        botApi.deleteMessage(
+            DeleteMessageRequest(
+                chatId = appProps.receiverChatId,
+                messageId = getCheckMessageId(update)
+            )
+        )
+    }
+}
