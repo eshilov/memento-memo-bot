aa84ec2d7fd2bbce4fb5ddd894a5106322c455e8
Evgeny Shilov <evgeny.shilov.225@gmail.com>
2022-05-03 22:02:06 +0500

    Include callback query attribute for update dto

--- src/main/kotlin/dev/eshilov/mementomemobot/telegram/dto/Update.kt
+++ src/main/kotlin/dev/eshilov/mementomemobot/telegram/dto/Update.kt
@@ -2,5 +2,6 @@ package dev.eshilov.mementomemobot.telegram.dto
 
 data class Update(
     val updateId: Long,
-    val message: Message?
+    val message: Message?,
+    val callbackQuery: CallbackQuery?
 )
