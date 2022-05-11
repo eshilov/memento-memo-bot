f7c913c5bf80add84f6b828572d9e950a8f01925
Evgeny Shilov <60179259+eshilov@users.noreply.github.com>
2022-05-11 22:48:02 +0500

    Clean previous notification after new notification is sent

--- src/main/kotlin/dev/eshilov/mementomemobot/remind/RemindScheduler.kt
+++ src/main/kotlin/dev/eshilov/mementomemobot/remind/RemindScheduler.kt
@@ -3,6 +3,8 @@ package dev.eshilov.mementomemobot.remind
 import dev.eshilov.mementomemobot.config.AppProps
 import dev.eshilov.mementomemobot.meme.MemeEntity
 import dev.eshilov.mementomemobot.meme.MemeRepository
+import dev.eshilov.mementomemobot.telegram.model.DeleteMessageRequest
+import dev.eshilov.mementomemobot.telegram.operation.BotApi
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -12,7 +14,8 @@ import java.time.ZonedDateTime.now
 class RemindScheduler(
     private val memeRepository: MemeRepository,
     private val appProps: AppProps,
-    private val memeNotifier: MemeNotifier
+    private val memeNotifier: MemeNotifier,
+    private val botApi: BotApi
 ) {
     companion object {
         private val logger = LoggerFactory.getLogger(RemindScheduler::class.java)
@@ -26,7 +29,11 @@ class RemindScheduler(
         meme?.let {
             logger.info("Reminding about meme ${meme.id}")
 
+            val previousNotificationId = meme.lastCheckMessageId
             memeNotifier.notifyAboutMeme(meme, "Уже посмотрел мем от кошечки?")
+            if (previousNotificationId != null) {
+                deletePreviousNotification(previousNotificationId)
+            }
         }
             ?: logger.info("Meme to remind not found")
     }
@@ -34,5 +41,14 @@ class RemindScheduler(
     private fun findMemeToCheck(): MemeEntity? {
         val lastCheckedBoundary = now().minusMinutes(appProps.minRemindFrequencyMinutes)
         return memeRepository.findFirstByViewedIsFalseAndLastCheckedIsBeforeOrderByLastChecked(lastCheckedBoundary)
+    }
+
+    private fun deletePreviousNotification(id: Long) {
+        botApi.deleteMessage(
+            DeleteMessageRequest(
+                chatId = appProps.receiverChatId,
+                messageId = id
+            )
+        )
     }
 }
